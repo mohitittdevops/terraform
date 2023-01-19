853dd127aecd4ebedeb35edeed33a99f3f3fa267
mohitittdevops <mohit.mathur@intimetec.com>
2023-01-19 15:08:49 +0530

    make changes in aws provider

--- aws_eip.tf
+++ aws_eip.tf
@@ -1,10 +1,10 @@
-# #Create an Elastic IP
-# resource "aws_eip" "lb" {
-#   vpc = true
-# }
+#Create an Elastic IP
+resource "aws_eip" "lb" {
+  vpc = true
+}
 
-# #Associate EIP with EC2 Instance
-# resource "aws_eip_association" "eip-association" {
-#   instance_id   = aws_instance.s1.id
-#   allocation_id = aws_eip.lb.id
-# }
+#Associate EIP with EC2 Instance
+resource "aws_eip_association" "eip-association" {
+  instance_id   = aws_instance.s1.id
+  allocation_id = aws_eip.lb.id
+}
